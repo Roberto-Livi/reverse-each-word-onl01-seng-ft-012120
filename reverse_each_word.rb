1bd35e826aef73955d139665024feff2ff349ddd
Roberto Livi <robertomlivi@outlook.com>
2020-01-24 13:59:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,4 +7,15 @@ def reverse_each_word(string)
   puts reverse.join(" ")
 end
 
-def reverse_
+def reverse_collect_word(string)
+  split_string = string.split(" ")
+  reverse = []
+  split_string.collect do |index|
+    reverse << index.reverse
+  end
+  puts reverse.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
+reverse_collect_word("Hello there, and how are you?")
